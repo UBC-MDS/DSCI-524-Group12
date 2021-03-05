ef7acccf9f1deeacaa3936ab0c16b07082996c2d
Mai Le <lephanthuymai@gmail.com>
2021-03-05 12:52:08 +0700

    fix testing approach for explore_clustering

--- datascience_eda/datascience_eda.py
+++ datascience_eda/datascience_eda.py
@@ -112,7 +112,7 @@ def get_clustering_default_hyperparameters():
     return clustering_default_hyperparameters
 
 
-def plot_pca_clusters(data, labels):
+def plot_pca_clusters(data, labels, random_state=None):
     """Carries out dimensionality reduction on the data for visualization, apdated from Lecture 2
 
     Parameters
@@ -121,13 +121,15 @@ def plot_pca_clusters(data, labels):
         the dataset
     labels : list
         list of labels predicted
+    random_state : int, optional
+        a number determines random number generation for centroid initialization, by default None
 
     Returns
     -------
     matplotlib.axes.AxesSubPlot
         the PCA plot
     """
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=2, random_state=random_state)
     principal_comp = pca.fit_transform(data)
     pca_df = pd.DataFrame(
         data=principal_comp, columns=["pca1", "pca2"], index=data.index
@@ -146,6 +148,220 @@ def plot_pca_clusters(data, labels):
 # endregion
 
 # region clustering functions
+
+
+def explore_KMeans_clustering(
+    df,
+    num_cols=None,
+    n_clusters=range(3, 5),
+    include_silhouette=True,
+    include_PCA=True,
+    random_state=None,
+):
+    """create, fit and plot KMeans clustering on the dataset
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        the dataset, should be transformed with StandardScaler
+    num_cols : list, optional
+        list of numeric column names, in case of None, get all numeric columns
+    metric : str, optional
+        metric, by default "euclidean"
+    n_clusters : list, optional
+        list of n_clusters hyperparams, by default range(2, 9)
+    include_silhouette : bool, optional
+        whether Silhouette plots should be generated, by default True
+    include_PCA : bool, optional
+        whether PCA plots should be generated, by default True
+    random_state : int, optional
+        a number determines random number generation for centroid initialization, by default None
+
+    Returns
+    -------
+    dict
+        a dictionary with key=type of plot, value=list of plots
+
+    Examples
+    -------
+    >>> original_df = pd.read_csv("/data/menu.csv")
+    >>> numeric_features = eda.get_numeric_columns(original_df)
+    >>> numeric_transformer = make_pipeline(SimpleImputer(), StandardScaler())
+    >>> preprocessor = make_column_transformer(
+    >>>     (numeric_transformer, numeric_features)
+    >>> )
+    >>> df = pd.DataFrame(
+    >>>     data=preprocessor.fit_transform(original_df), columns=numeric_features
+    >>> )
+    >>> explore_KMeans_clusterting(df)
+    """
+    if num_cols is None:
+        num_cols = get_numeric_columns(df)
+    else:
+        _verify_numeric_cols(df, num_cols)
+    x = df[num_cols]
+    results = {}
+    if 1 in n_clusters:
+        raise Exception("n_cluster cannot be 1")
+
+    print("------------------------")
+    print("K-MEANS CLUSTERING")
+    print("------------------------")
+
+    if len(n_clusters) > 1:
+        print("Generating KElbow plot for KMeans.")
+        # visualize using KElbowVisualizer
+        kmeans = KMeans(random_state=random_state)
+
+        plt.clf()
+        fig, ax = plt.subplots()
+        elbow_visualizer = KElbowVisualizer(kmeans, k=n_clusters, ax=ax)
+        elbow_visualizer.fit(x)  # Fit the data to the visualizer
+        elbow_visualizer.show()
+        plt.close()
+        elbow_visualizer.k = elbow_visualizer.elbow_value_  # fix printing issue
+        results["KElbow"] = fig
+    else:
+        results["KElbow"] = None
+
+    # visualize using SilhouetteVisualizer
+    print("Generating Silhouette & PCA plots")
+    silhouette_plots = []
+    pca_plots = []
+    for k in n_clusters:
+        print(f"Number of clusters: {k}")
+
+        kmeans = KMeans(k, random_state=random_state)
+
+        if include_silhouette:
+            fig, ax = plt.subplots()
+            s_visualizer = SilhouetteVisualizer(kmeans, colors="yellowbrick", ax=ax)
+            s_visualizer.fit(x)  # Fit the data to the visualizer
+            s_visualizer.show()
+            silhouette_plots.append(fig)
+            # plt.clf()
+            plt.close()
+
+        else:
+            silhouette_plots.append(None)
+
+        # PCA plots
+        if include_PCA:
+            labels = kmeans.fit_predict(x)
+            pca_fig = plot_pca_clusters(x, labels, random_state=random_state)
+            pca_plots.append(pca_fig)
+        else:
+            pca_plots.append(None)
+
+    results["Silhouette"] = silhouette_plots
+    results["PCA"] = pca_plots
+
+    return results
+
+
+def explore_DBSCAN_clustering(
+    df,
+    num_cols=None,
+    metric="euclidean",
+    eps=[0.5],
+    min_samples=[5],
+    include_silhouette=True,
+    include_PCA=True,
+    random_state=None,
+):
+    """fit and plot DBSCAN clustering algorithms
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        the dataset, should be transformed with StandardScaler
+    num_cols : list, optional
+        list of numeric column names, in case of None, get all numeric columns
+    metric : str, optional
+        metric, by default "euclidean"
+    eps : list, optional
+        list of eps hyperparams, by default [0.5]
+    min_samples: list, optional
+        list of min_samples hyperparams, by default [5]
+    include_silhouette : bool, optional
+        whether Silhouette plots should be generated, by default True
+    include_PCA : bool, optional
+        whether PCA plots should be generated, by default True
+    random_state : int, optional
+        a number determines random number generation for centroid initialization, by default None
+
+    Returns
+    -------
+    Tuple
+        list
+            a list of n_clusters values returned by DBSCAN models
+        dict
+            a dictionary with key=type of plot, value=list of plots
+
+    Examples
+    -------
+    >>> original_df = pd.read_csv("/data/menu.csv")
+    >>> numeric_features = eda.get_numeric_columns(original_df)
+    >>> numeric_transformer = make_pipeline(SimpleImputer(), StandardScaler())
+    >>> preprocessor = make_column_transformer(
+    >>>     (numeric_transformer, numeric_features)
+    >>> )
+    >>> df = pd.DataFrame(
+    >>>     data=preprocessor.fit_transform(original_df), columns=numeric_features
+    >>> )
+    >>> n_clusters, dbscan_plots = explore_DBSCAN_clusterting(df)
+    """
+    if num_cols is None:
+        num_cols = get_numeric_columns(df)
+    else:
+        _verify_numeric_cols(df, num_cols)
+
+    x = df[num_cols]
+
+    results = {}
+    n_clusters = []
+
+    s_plots = []
+    pca_plots = []
+
+    print("------------------------")
+    print("DBSCAN CLUSTERING")
+    print("------------------------")
+
+    for e in eps:
+        for ms in min_samples:
+            dbscan = DBSCAN(eps=e, min_samples=ms, metric=metric)
+            dbscan.fit(x)
+            k = len(set(dbscan.labels_)) - 1  # exclduing -1 labels
+            n_clusters.append(k)
+            print(f"eps={e}, min_samples={ms}, n_cluster={k}")
+            if include_silhouette and k > 0:
+                # generat Silhouette plot
+                dbscan.n_clusters = k
+                dbscan.predict = lambda x: dbscan.labels_
+                fig, ax = plt.subplots()
+                s_visualizer = SilhouetteVisualizer(dbscan, colors="yellowbrick", ax=ax)
+                s_visualizer.fit(x)
+                s_visualizer.show()
+                s_plots.append(fig)
+                # plt.clf()
+                plt.close()
+            else:
+                s_plots.append(None)
+
+            if include_PCA:
+                # genrate PCA plot
+                p_lot = plot_pca_clusters(x, dbscan.labels_, random_state=random_state)
+                pca_plots.append(p_lot)
+            else:
+                pca_plots.append(None)
+
+    results["Silhouette"] = s_plots
+    results["PCA"] = pca_plots
+
+    return n_clusters, results
+
+
 def explore_clustering(
     df,
     numeric_cols=None,
@@ -157,11 +373,19 @@ def explore_clustering(
     Parameters
     ----------
     df : pandas.DataFrame
-        the dataset (X), should already transformed with StandardScaler
+        the dataset (X), should already be transformed with StandardScaler
     numeric_cols : list, optional
         a list of numeric columns used for clustering, by default None, will be assigned with all numeric columns
     hyperparameter_dict : dict, optional
-        the hyperparameters to be used in the clustering algorithms, by default get_clustering_default_hyperparameters()
+        the hyperparameters to be used in the clustering algorithms, by default use
+        {
+            "KMeans": {"n_clusters": range(2, 6)},
+            "DBSCAN": {
+                "eps": [0.5],
+                "min_samples": [5],
+                "distance_metric": "euclidean",
+            }
+        }
     random_state : int, optional
         a number determines random number generation for centroid initialization, by default None
 
@@ -172,12 +396,11 @@ def explore_clustering(
 
     Examples
     -------
-    >>> original_df = pd.read_csv(currentdir + "/data/menu.csv")
+    >>> original_df = pd.read_csv("data/menu.csv")
     >>> numeric_features = eda.get_numeric_columns(original_df)
-    >>> drop_features = []
     >>> numeric_transformer = make_pipeline(SimpleImputer(), StandardScaler())
     >>> preprocessor = make_column_transformer(
-    >>>     (numeric_transformer, numeric_features), ("drop", drop_features)
+    >>>     (numeric_transformer, numeric_features)
     >>> )
     >>> df = pd.DataFrame(
     >>>     data=preprocessor.fit_transform(original_df), columns=numeric_features
@@ -212,7 +435,7 @@ def explore_clustering(
 
     dbscan_params = hyperparameter_dict["DBSCAN"]
     if not ("eps" in dbscan_params):
-        raise Exception("Excepting eps in DBSCAN's hyperparams.")
+        raise Exception("Expecting eps in DBSCAN's hyperparams.")
 
     if not ("min_samples" in dbscan_params):
         raise Exception("Expecting min_samples in DBSCAN's hyperparams.")
@@ -255,212 +478,6 @@ def explore_clustering(
     print("***********************")
 
     return result
-
-
-def explore_KMeans_clustering(
-    df,
-    num_cols=None,
-    n_clusters=range(3, 5),
-    include_silhouette=True,
-    include_PCA=True,
-    random_state=None,
-):
-    """create, fit and plot KMeans clustering on the dataset
-
-    Parameters
-    ----------
-    df : pandas.DataFrame
-        the dataset, should be transformed with StandardScaler
-    num_cols : list, optional
-        list of numeric column names, in case of None, get all numeric columns
-    metric : str, optional
-        metric, by default "euclidean"
-    n_clusters : list, optional
-        list of n_clusters hyperparams, by default range(2, 9)
-    include_silhouette : bool, optional
-        whether Silhouette plots should be generated, by default True
-    include_PCA : bool, optional
-        whether PCA plots should be generated, by default True
-    random_state : int, optional
-        a number determines random number generation for centroid initialization, by default None
-
-    Returns
-    -------
-    dict
-        a dictionary with key=type of plot, value=list of plots
-
-    Examples
-    -------
-    >>> original_df = pd.read_csv(currentdir + "/data/menu.csv")
-    >>> numeric_features = eda.get_numeric_columns(original_df)
-    >>> drop_features = []
-    >>> numeric_transformer = make_pipeline(SimpleImputer(), StandardScaler())
-    >>> preprocessor = make_column_transformer(
-    >>>     (numeric_transformer, numeric_features), ("drop", drop_features)
-    >>> )
-    >>> df = pd.DataFrame(
-    >>>     data=preprocessor.fit_transform(original_df), columns=numeric_features
-    >>> )
-    >>> explore_KMeans_clusterting(df)
-    """
-    if num_cols is None:
-        num_cols = get_numeric_columns(df)
-    else:
-        _verify_numeric_cols(df, num_cols)
-    x = df[num_cols]
-    results = {}
-    if 1 in n_clusters:
-        raise Exception("n_cluster cannot be 1")
-
-    print("------------------------")
-    print("K-MEANS CLUSTERING")
-    print("------------------------")
-
-    if len(n_clusters) > 1:
-        print("Generating KElbow plot for KMeans.")
-        # visualize using KElbowVisualizer
-        kmeans = KMeans(random_state=random_state)
-        elbow_visualizer = KElbowVisualizer(kmeans, k=n_clusters)
-        elbow_visualizer.fit(x)  # Fit the data to the visualizer
-        elbow_visualizer.show()
-        plt.close()
-        elbow_visualizer.k = elbow_visualizer.elbow_value_  # fix printing issue
-        results["KElbow"] = elbow_visualizer
-    else:
-        results["KElbow"] = None
-
-    # visualize using SilhouetteVisualizer
-    print("Generating Silhouette & PCA plots")
-    silhouette_plots = []
-    pca_plots = []
-    for k in n_clusters:
-        print(f"Number of clusters: {k}")
-
-        kmeans = KMeans(k, random_state=random_state)
-
-        if include_silhouette:
-            s_visualizer = SilhouetteVisualizer(kmeans, colors="yellowbrick")
-            s_visualizer.fit(x)  # Fit the data to the visualizer
-            silhouette_plots.append(s_visualizer)
-            s_visualizer.show()
-            # plt.clf()
-            plt.close()
-
-        else:
-            silhouette_plots.append(None)
-
-        # PCA plots
-        if include_silhouette:
-            labels = kmeans.fit_predict(x)
-            pca_fig = plot_pca_clusters(x, labels)
-            pca_plots.append(pca_fig)
-        else:
-            pca_plots.append(None)
-
-    results["Silhouette"] = silhouette_plots
-    results["PCA"] = pca_plots
-
-    return results
-
-
-def explore_DBSCAN_clustering(
-    df,
-    num_cols=None,
-    metric="euclidean",
-    eps=[0.5],
-    min_samples=[5],
-    include_silhouette=True,
-    include_PCA=True,
-):
-    """fit and plot DBSCAN clustering algorithms
-
-    Parameters
-    ----------
-    df : pandas.DataFrame
-        the dataset, should be transformed with StandardScaler
-    num_cols : list, optional
-        list of numeric column names, in case of None, get all numeric columns
-    metric : str, optional
-        metric, by default "euclidean"
-    eps : list, optional
-        list of eps hyperparams, by default [0.5]
-    min_samples: list, optional
-        list of min_samples hyperparams, by default [5]
-    include_silhouette : bool, optional
-        whether Silhouette plots should be generated, by default True
-    include_PCA : bool, optional
-        whether PCA plots should be generated, by default True
-
-    Returns
-    -------
-    Tuple
-        list
-            a list of n_clusters values returned by DBSCAN models
-        dict
-            a dictionary with key=type of plot, value=list of plots
-
-    Examples
-    -------
-    >>> original_df = pd.read_csv(currentdir + "/data/menu.csv")
-    >>> numeric_features = eda.get_numeric_columns(original_df)
-    >>> drop_features = []
-    >>> numeric_transformer = make_pipeline(SimpleImputer(), StandardScaler())
-    >>> preprocessor = make_column_transformer(
-    >>>     (numeric_transformer, numeric_features), ("drop", drop_features)
-    >>> )
-    >>> df = pd.DataFrame(
-    >>>     data=preprocessor.fit_transform(original_df), columns=numeric_features
-    >>> )
-    >>> n_clusters, dbscan_plots = explore_DBSCAN_clusterting(df)
-    """
-    if num_cols is None:
-        num_cols = get_numeric_columns(df)
-    else:
-        _verify_numeric_cols(df, num_cols)
-
-    x = df[num_cols]
-
-    results = {}
-    n_clusters = []
-
-    s_plots = []
-    pca_plots = []
-
-    print("------------------------")
-    print("DBSCAN CLUSTERING")
-    print("------------------------")
-
-    for e in eps:
-        for ms in min_samples:
-            dbscan = DBSCAN(eps=e, min_samples=ms, metric=metric)
-            dbscan.fit(x)
-            k = len(set(dbscan.labels_)) - 1  # exclduing -1 labels
-            n_clusters.append(k)
-            print(f"eps={e}, min_samples={ms}, n_cluster={k}")
-            if include_silhouette and k > 0:
-                # generat Silhouette plot
-                dbscan.n_clusters = k
-                dbscan.predict = lambda x: dbscan.labels_
-                s_visualizer = SilhouetteVisualizer(dbscan, colors="yellowbrick")
-                s_plots.append(s_visualizer)
-                s_visualizer.fit(x)
-                s_visualizer.show()
-                # plt.clf()
-                plt.close()
-            else:
-                s_plots.append(None)
-
-            if include_PCA:
-                # genrate PCA plot
-                p_lot = plot_pca_clusters(x, dbscan.labels_)
-                pca_plots.append(p_lot)
-            else:
-                pca_plots.append(None)
-
-    results["Silhouette"] = s_plots
-    results["PCA"] = pca_plots
-
-    return n_clusters, results
 
 
 # endregion
